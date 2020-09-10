5d03a17e8b84431e409b46c3a56208262d53ae44
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2020-09-10 22:56:53 +0300

    Implement iterator

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-null;
+Number.prototype[Symbol.iterator] = function numberIterator() {
+	let current = 0
+	const last = this.valueOf();
+
+	return {
+		[Symbol.iterator]() {
+			return this;
+		},
+		next() {
+			const done = current >= last;
+			const value = done ? undefined : current;
+
+			current++;
+
+			return { value, done };
+		},
+	};
+};
